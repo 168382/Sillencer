93000526ec2bee55a22c35890d9a95055f673b7d
sillencer <vincent.rimba@strathmore.edu>
2024-06-19 14:23:27 +0300

    nav

---
+++ templates/nav.php
@@ -0,0 +1,17 @@
+<div class="navbar">
+        <div class="topnav">
+         <a href="page.php">Home</a>
+         <a href="about.php">About us</a>
+         <a href="service.php">Services</a>
+         <a href="form.php">Order</a>
+         <a href="contact.php">Contact us</a>
+         <a href="chanel.php">channel</a>
+         <a href="feedback.php">Feedback</a>
+        
+         <div class="topnav-right">
+             <a href="signin.php">sign in</a>
+             <a href="signup.php">sign up</a>
+         </div>    
+     
+     
+   </div>  
