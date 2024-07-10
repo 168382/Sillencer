2d68a27269d0c46acc57c99e8f0bc455ea0a9267
sillencer <vincent.rimba@strathmore.edu>
2024-07-10 06:55:53 +0300

    forms

--- ProjectX/form.php
+++ ProjectX/form.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-
-
+<?php require_once("templates/header.php");?>
 <?php include_once("templates/nav.php");?>
+
      <div class="main">
         <div class="header">
           <h1>YOUR FOOD ORDER?</h1>
