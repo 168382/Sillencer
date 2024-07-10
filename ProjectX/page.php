5c34e99cbac8abd9802fa0d397debc9883898079
sillencer <vincent.rimba@strathmore.edu>
2024-07-10 06:56:40 +0300

    page

--- ProjectX/page.php
+++ ProjectX/page.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FOODSTUFF</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-
-
+<?php require_once("templates/header.php");?>
 <?php include_once("templates/nav.php");?>
+
+
     <div class="main">
         <div class="home">
             <h1>WEX RESTARUANT</h1>
