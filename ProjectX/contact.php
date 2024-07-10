3da84ea768157bd8f23ef61bbb6a7788b1079fcc
sillencer <vincent.rimba@strathmore.edu>
2024-07-10 06:54:08 +0300

    contacts

--- ProjectX/contact.php
+++ ProjectX/contact.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contact us </title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-
-
+<?php require_once("templates/header.php");?>
 <?php include_once("templates/nav.php");?>
+ 
 </body>
 <h1>WEX RESTARUANT</h1><br><br>
 <h2>Contact Us</h2>
