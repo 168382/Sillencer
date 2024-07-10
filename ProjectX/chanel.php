5d99b3a8bf5bcb9c09e69f4453ae8b4b8a8ae871
sillencer <vincent.rimba@strathmore.edu>
2024-07-10 06:52:37 +0300

    channel

--- ProjectX/chanel.php
+++ ProjectX/chanel.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>channel</title>
-    <link rel="stylesheet" href="../css/style.css">
-</head>
-<body>
-
-
+<?php require_once("templates/header.php")?>
 <?php include_once("templates/nav.php");?>
+
 </body>
 <p>WELLCOME TO WEX CHANNEL </p>
 <b>click the link below</b>
